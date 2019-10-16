3de2a7414e10616e1de5c0483a835b4ad6de7d89
Cristian Barrios <c.barriosespinosa@student.maastricht.nl>
2019-10-16 14:18:37 +0200

    add smiles of all the molecules

---
+++ AllSmilesQuery.R
@@ -0,0 +1,12 @@
+library(WikidataQueryServiceR)
+
+query <- 'SELECT DISTINCT ?compound ?smiles ?isoSmiles WHERE {
+  ?compound wdt:P233 | wdt:P2017 [] .
+  OPTIONAL { ?compound wdt:P233 ?smiles }
+  OPTIONAL { ?compound wdt:P2017 ?isoSmiles }
+}'
+
+r <- query_wikidata(query)
+
+write.table(r, file = "AllSmilesWikidata.tsv", sep = "\t", row.names = FALSE,
+            quote = F)
